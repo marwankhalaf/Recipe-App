98d2e8363f0ddcd45255a0066f20e54155746642
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2023-04-07 04:05:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// start logic
+
+
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
